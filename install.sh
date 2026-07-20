c1f68096055017f5958c5508bc48a9eb073e0e24
koyu <me@koyu.space>
2026-07-20 09:40:29 +0200

    Fully use ninja for installing

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-./build.sh
+cmake -B build -S . \\
+    -GNinja \\
+    -DCMAKE_BUILD_TYPE=Release \\
+    -DCMAKE_INSTALL_PREFIX=/usr
+ninja -C build
 sudo rpm-ostree usroverlay
 sudo ninja -C build install
